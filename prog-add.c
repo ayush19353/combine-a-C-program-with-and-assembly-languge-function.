7ccc7c2271f80e4c1bb1e0f9b093e9aa59ca9f20
ayush19353 <58385978+ayush19353@users.noreply.github.com>
2020-12-28 17:39:27 +0530

    Add files via upload

---
+++ prog-add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+// #include<inttypes.h>
+
+int add(int a,int b);
+
+
+int main()
+{
+	/* code */
+	int a,b;
+	scanf("%d%d",&a,&b);
+    
+    printf("%d\n",add(a,b));
+	return 0;
+
+}
